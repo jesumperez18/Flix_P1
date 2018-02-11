e0dfbfdcf5ddbb916d804602ed3190670579d2b2
jesumperez18 <jesuperez@csumb.edu>
2018-02-11 10:21:51 -0800

    complete

---
+++ flix/ViewControllers/SuperheroViewController.swift
@@ -0,0 +1,85 @@
+//
+//  SuperheroViewController.swift
+//  flix
+//
+//  Created by Jesus perez on 2/7/18.
+//  Copyright © 2018 Jesus perez. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    var movies: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
+        fetchMovies()
+        
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+        let movie = movies[indexPath.item]
+        if let posterPathString = movie["poster_path"] as? String{
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            let posterURL = URL(string: baseURLString + posterPathString)!
+            cell.posterImageView.af_setImage(withURL: posterURL)
+            
+        }
+        
+        return cell
+        
+    }
+    func fetchMovies()
+    {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=7113d80fa3248ad7e79003f590358cea")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // this will run when network request returns
+            if let error = error{
+                print(error.localizedDescription)
+            }else if let data = data{
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                //print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String:Any]]
+                
+                self.movies = movies
+                self.collectionView.reloadData()
+                //self.refreshControl.endRefreshing()
+                
+            }
+        }
+        task.resume()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Jesus perez on 2/7/18.
+//  Copyright © 2018 Jesus perez. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
